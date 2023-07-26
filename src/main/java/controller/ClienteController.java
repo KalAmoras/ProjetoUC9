e28df2e78d097519951f0ac329f0416fc070d646
KalAmoras <kalamorasarquivo@gmail.com>
2023-07-25 21:07:07 -0300

    ClientController added

---
+++ src/main/java/controller/ClienteController.java
@@ -0,0 +1,157 @@
+package controller;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import dao.ClienteDAO;
+import models.Cliente;
+
+
+public class ClienteController extends HttpServlet {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private ClienteDAO clienteDAO;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        clienteDAO = new ClienteDAO();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "list";
+        }
+
+        switch (action) {
+            case "list":
+                listClients(request, response);
+                break;
+            case "edit":
+                editClient(request, response);
+                break;
+            case "delete":
+                deleteClient(request, response);
+                break;
+            default:
+                listClients(request, response);
+        }
+    }
+
+    
+
+	@Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action != null) {
+            switch (action) {
+                case "add":
+					try {
+						addClient(request, response);			
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+                    break;
+                case "update":
+                	try {
+                    updateClient(request, response);
+                	} catch (IOException e) {
+                		e.printStackTrace();
+					}
+                    break;
+                default:
+                    listClients(request, response);
+            }
+        }
+    }
+    
+    
+    
+
+    private void  listClients(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Cliente> clientes = clienteDAO.getAll();
+        request.setAttribute("clientes", clientes);
+        request.getRequestDispatcher("/jsp/listAlunos.jsp").forward(request, response);
+    }
+    
+    
+    private void addClient(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, SQLException, ServletException {
+        int matricula = Integer.parseInt(request.getParameter("matricula"));
+        String nome = request.getParameter("nome");
+        String endereco = request.getParameter("endereco");
+        String modalidade = request.getParameter("modalidade");
+
+        Cliente newClient = new Cliente();
+        newClient.setMatricula(matricula);
+        newClient.setNome(nome);
+        newClient.setEndereco(endereco);
+        newClient.setModalidade(modalidade);
+
+        clienteDAO.insertClient(newClient);
+
+        response.sendRedirect("ClienteController?action=list");
+    }
+
+   
+
+    private void editClient(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int matricula = Integer.parseInt(request.getParameter("matricula"));
+        Cliente cliente = clienteDAO.getById(matricula);
+
+        request.setAttribute("cliente", cliente);
+        request.getRequestDispatcher("/jsp/editCliente.jsp").forward(request, response);
+    }
+
+    private void updateClient(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        int matricula = Integer.parseInt(request.getParameter("matricula"));
+        String nome = request.getParameter("nome");
+        String endereco = request.getParameter("endereco");
+        String modalidade = request.getParameter("modalidade");
+        ClienteDAO clientDAO = new ClienteDAO();
+
+        Cliente updatedClient = new Cliente();
+        updatedClient.setMatricula(matricula);
+        updatedClient.setNome(nome);
+        updatedClient.setEndereco(endereco);
+        updatedClient.setModalidade(modalidade);
+
+        clientDAO.updateClient(updatedClient);
+
+        response.sendRedirect("ClienteController");
+    }
+
+    Cliente client = new ClienteDAO();
+	ClienteDAO clientDAO = new ClienteDAO();
+	
+	private void deleteClient(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		int matricula = Integer.parseInt(request.getParameter("matricula"));
+		clientDAO.deleteClient(matricula);
+	}
+	
+	/* private void deleteAluno(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        int matricula = Integer.parseInt(request.getParameter("matricula"));
+        alunoDAO.deleteAluno(matricula);
+        response.sendRedirect("AlunoController");
+    }*/
+
+	
+	
+    
+    
+}
--- src/main/java/dao/ClienteDAO.java
+++ src/main/java/dao/ClienteDAO.java
@@ -9,22 +9,23 @@ import java.util.ArrayList;
 import connection.ConnectionMySQL;
 import models.Cliente;
 
-public class ClienteDAO extends Cliente { // Assume you have a connection object, you can get it from your database connection pool.
-    
+public class ClienteDAO extends Cliente { 
 	
 	private static final String INSERT_SQL = "INSERT INTO Cliente (matricula, nome, endereco, modalidade) VALUES (?, ?, ?, ?)";
 	private static final String UPDATE_SQL = "UPDATE Cliente SET matricula=?, nome=?, endereco=?, modalidade=? WHERE matricula=?";
     private static final String DELETE_SQL = "DELETE FROM Cliente WHERE matricula=?";
     private static final String SELECT_ALL_SQL = "SELECT * FROM Cliente";
+    private static final String SELECT_BY_ID_SQL = "SELECT * FROM Cliente WHERE matricula=?";
 
 
-    public void insertClient(Cliente cliente) throws SQLException {
+    public void insertClient(Cliente cliente) {
     	
     	Connection conn = ConnectionMySQL.iniciarConexao();
         
-    	PreparedStatement stmt = conn.prepareStatement(INSERT_SQL);
+    	PreparedStatement stmt = null;
     	
     	try {
+    			stmt = conn.prepareStatement(INSERT_SQL);
 		        stmt.setInt(1, cliente.getMatricula());
 		        stmt.setString(2, cliente.getNome());
 		        stmt.setString(3, cliente.getEndereco());
@@ -37,8 +38,9 @@ public class ClienteDAO extends Cliente { // Assume you have a connection object
         }
     }
     
-	public void updateClient(Cliente cliente) {
-	    
+    
+    
+	public void updateClient(Cliente cliente) {	    
 		
     	Connection conn = ConnectionMySQL.iniciarConexao();
         
@@ -59,7 +61,7 @@ public class ClienteDAO extends Cliente { // Assume you have a connection object
         }
 	 }
 	
-	public void deleteClient(Cliente cliente) {
+	public void deleteClient(int matricula) {
     	Connection conn = ConnectionMySQL.iniciarConexao();
     	
     	PreparedStatement stmt = null;
@@ -70,13 +72,12 @@ public class ClienteDAO extends Cliente { // Assume you have a connection object
 			stmt.executeUpdate();
 		} catch (SQLException erro) {
 			erro.printStackTrace();
-		} finally {
-		
+		} finally {		
 			ConnectionMySQL.encerrarConexao(conn, stmt);
 		}
 	}
 	
-	public ArrayList<Cliente> read()
+	public ArrayList<Cliente> getAll()
 	{
 		Connection conn = ConnectionMySQL.iniciarConexao();
     	
@@ -106,4 +107,35 @@ public class ClienteDAO extends Cliente { // Assume you have a connection object
 		return listaclientes;
 	}
 	
+	public Cliente getById(int matricula) {
+	    Connection conn = ConnectionMySQL.iniciarConexao();
+	    PreparedStatement stmt = null;
+	    ResultSet rs = null;
+	    Cliente cliente = null; 
+
+	    try {
+	        stmt = conn.prepareStatement(SELECT_BY_ID_SQL); //
+	        stmt.setInt(1, matricula);
+	        rs = stmt.executeQuery();
+
+	        if (rs.next()) {
+	            String nome = rs.getString("nome");
+	            String endereco = rs.getString("endereco");
+	            String modalidade = rs.getString("modalidade");
+
+	            cliente = new Cliente();
+	            cliente.setMatricula(matricula);
+	            cliente.setNome(nome);
+	            cliente.setEndereco(endereco);
+	            cliente.setModalidade(modalidade);
+	        }
+
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        ConnectionMySQL.encerrarConexao(conn, stmt, rs);
+	    }
+	    
+	    return cliente;
+	}
 }
